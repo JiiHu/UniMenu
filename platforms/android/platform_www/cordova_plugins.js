6201ebaff962609bf9d40eb725b400996c189946
Jonne <jonne.huotari@cs.helsinki.fi>
2015-12-31 11:21:33 +0200

    Whitelisting internet

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,20 @@ module.exports = [
         "clobbers": [
             "plugin.google.maps"
         ]
+    },
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-geolocation": "1.0.1",
-    "plugin.google.maps": "1.3.7"
+    "plugin.google.maps": "1.3.7",
+    "cordova-plugin-whitelist": "1.2.0"
 }
 // BOTTOM OF METADATA
 });
