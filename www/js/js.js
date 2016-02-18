303f86c2019adc151e6db1143e2bb8ab94b408f1
Jonne <jonne.huotari@cs.helsinki.fi>
2016-02-18 10:14:00 +0200

    Loading animation length

--- www/js/js.js
+++ www/js/js.js
@@ -350,7 +350,7 @@ $(document).ready(function(){
   tabby.init();
   appendShellitFooterIfBrowser();
 
-  $('#big-blue').delay(500).fadeOut(500);
+  $('#big-blue').delay(1000).fadeOut(500);
 
 });
 
